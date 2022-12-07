921ddd0a48021b92649b6c724dc58b1afb286de7
pranesh-rvitm <117212598+pranesh-rvitm@users.noreply.github.com>
2022-12-07 18:29:24 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ require("./db/conn");
 const hbs = require('hbs');
 const { registerPartial, registerPartials } = require('hbs');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 
 
 
